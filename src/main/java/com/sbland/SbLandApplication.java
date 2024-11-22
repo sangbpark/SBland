457926a33d854df97317bb7cac572fc422f44389
sangbpark <qkrtkdqo94@gmail.com>
2024-11-22 19:52:18 +0900

    초기 설정 약간의 홈페이지 마크업 20241122

---
+++ src/main/java/com/sbland/SbLandApplication.java
@@ -0,0 +1,13 @@
+package com.sbland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbLandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbLandApplication.class, args);
+	}
+
+}
